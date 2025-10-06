7caa30987127c3891b6ecb66035fb7df1a67462e
Josiah King <josiahaccounts@gmail.com>
2025-10-06 18:35:22 +0100

    setup first seeder and prisma users table migration

---
+++ prisma/migrations/20251006161149_users/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "public"."Users" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "public"."Users"("email");
---
+++ src/db/seeders/seed.user.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "../../generated/prisma/index"; 
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const hashed = await bcrypt.hash("password123", 10);
+  await prisma.users.upsert({
+    where: { email: "test@example.com" },
+    update: {},
+    create: {
+      email: "test@example.com",
+      first_name: "John",
+      last_name: "Doe",
+      password: hashed,
+    },
+  });
+}
+
+main()
+  .then(() => console.log("Seeding done"))
+  .catch((e) => console.error(e))
+  .finally(async () => await prisma.$disconnect());
